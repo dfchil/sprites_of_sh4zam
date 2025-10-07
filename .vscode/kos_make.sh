d9b50f4d0e15891fc41bf0affe03324a501de75b
Daniel Fairchild <daniel@viamap.net>
2025-10-07 18:29:32 +0200

    added .vscode setup improved perspective matrix

---
+++ .vscode/kos_make.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+#set the KOS environtment variables
+source /opt/toolchains/dc/kos/environ.sh
+DCTRACE=1 DEBUG=1 BASEPATH="/pc" make clean
+# SINGLEDEMO=0 DCPROF=1 SINGLEDEMO=7 
+SHOWFRAMETIMES=1 DEBUG=1 OPTLEVEL=g BASEPATH=/pc make -j 44
+find . -name '*.o' -exec rm {} \; 
+exit
